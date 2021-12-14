2119bab4855857056d8e62cdac22e7b643454ec2
Angela Loftus <angelaloftus@yahoo.com>
2021-12-14 00:29:19 -0600

    updated manifest.json, added screenshot

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,15 @@
-const APP_PREFIX = 'FoodEvent-';     
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
-// const FILES_TO_CACHE = [
-//   "./index.html",
-//   "./events.html",
-//   "./tickets.html",
-//   "./schedule.html",
-//   "./assets/css/style.css",
-//   "./assets/css/bootstrap.css",
-//   "./assets/css/tickets.css",
-//   "./dist/app.bundle.js",
-//   "./dist/events.bundle.js",
-//   "./dist/tickets.bundle.js",
-//   "./dist/schedule.bundle.js"
-// ];
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./manifest.json",
+    "./idb.js",
+    "./service-workers.js",
+    "./js/index.js",
+    "../server.js"
+];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
